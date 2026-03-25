a4210b679f3eb566bab025d81ab5014609b66b8d
John Kamau <jnkamau.dev@gmail.com>
2026-03-25 15:59:31 +0300

    feat: add changes

--- eight.js
+++ eight.js
@@ -1,1 +1,18 @@
 //Write a JavaScript program to check whether all the digits in a given number are the same or not.
+
+function allDigitsSame(num) {
+    let lastDigit = num % 10; 
+
+    while (num > 0) {
+        if (num % 10 !== lastDigit) {
+            return false;
+        }
+        num = Math.floor(num / 10);
+    }
+
+    return true;
+}
+
+
+console.log(allDigitsSame(1111)); 
+console.log(allDigitsSame(1234)); 
--- five.js
+++ five.js
@@ -1,1 +1,25 @@
 //Write a JavaScript program to create a new string from a given string by changing the position of the first and last characters. The string length must be broader than or equal to 1.
+
+
+function swapfirstLast(str){
+    if(str.length<= 1){
+        return str;
+    }
+
+    let first = str.charAt(0);
+    let last = str.charAt(str.length-1);
+    let middle = str.slice(1, -1);
+
+    return last + middle + first;
+}
+
+console.log(swapfirstLast("Mother"));
+console.log(swapfirstLast("fear"));
+console.log(swapfirstLast("w"));
+console.log(swapfirstLast("to"));
+
+
+
+
+//strings must be enclosed in single or double quotes.
+//use .slice for extracting part of a string.
